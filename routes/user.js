3791c755e81b37d1256f073e43e2252903fcea60
Theres Mary <theres.mec@gmail.com>
2020-11-11 08:37:26 +0530

    login session

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,11 @@ var userHelpers = require('../helpers/user-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let user=req.session.user;
+  console.log(user)
   productHelpers.getAllProducts().then((products)=>{
-    console.log(products)
-    res.render('user/view-products', { title: 'BOOTS', products});
+    //console.log(products)
+    res.render('user/view-products', { title: 'BOOTS', products,user});
   });
 });
 
@@ -31,12 +33,19 @@ router.post('/login',(req,res)=>{
   userHelpers.doLogin(req.body).then((response)=>{
     console.log(response)
     if(response.loginStatus){
+       req.session.loggedIn = true;
+       req.session.user = response.user
        res.redirect('/')
     }
     else{
       res.redirect('/login')
     }
   })
+});
+
+router.get('/logout',(req,res)=>{
+  req.session.destroy();
+  res.render('user/user-login')
 });
 
 
